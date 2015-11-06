320a28e4fcfc5a2ef7972e34f2bc3a61375a2683
tony.cao <tonycao@augmentum.com.cn>
2015-11-06 18:22:06 +0800

    Add view --IconButton

---
+++ teardroid/src/main/java/com/tony/teardroid/view/IconButton.java
@@ -0,0 +1,111 @@
+package com.tony.teardroid.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+import com.tony.teardroid.R;
+
+/**
+ * 图标按钮 可以设置图标和文字的间距
+ * xml中需要设置ns 并且只能设置左右
+ * 特别感谢https://github.com/pnc/IconButton
+ */
+public class IconButton extends Button {
+    protected int drawableWidth;
+    protected DrawablePositions drawablePosition;
+    protected int iconPadding;
+
+    // Cached to prevent allocation during onLayout
+    Rect bounds;
+
+    private enum DrawablePositions {
+        NONE,
+        LEFT_AND_RIGHT,
+        LEFT,
+        RIGHT
+    }
+
+    public IconButton(Context context) {
+        super(context);
+        bounds = new Rect();
+    }
+
+    public IconButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        bounds = new Rect();
+        applyAttributes(attrs);
+    }
+
+    public IconButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        bounds = new Rect();
+        applyAttributes(attrs);
+    }
+
+    protected void applyAttributes(AttributeSet attrs) {
+        // Slight contortion to prevent allocating in onLayout
+        if (null == bounds) {
+            bounds = new Rect();
+        }
+
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.IconButton);
+        int paddingId = typedArray.getDimensionPixelSize(R.styleable.IconButton_iconPadding, 0);
+        setIconPadding(paddingId);
+        typedArray.recycle();
+    }
+
+    public void setIconPadding(int padding) {
+        iconPadding = padding;
+        requestLayout();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        //获取文字
+        Paint textPaint = getPaint();
+        String text = getText().toString();
+        textPaint.getTextBounds(text, 0, text.length(), bounds);
+        int textWidth = bounds.width();
+        int factor = (drawablePosition == DrawablePositions.LEFT_AND_RIGHT) ? 2 : 1;
+        int contentWidth = drawableWidth + iconPadding * factor + textWidth;
+        int horizontalPadding = (int) ((getWidth() / 2.0) - (contentWidth / 2.0));
+        setCompoundDrawablePadding(-horizontalPadding + iconPadding);
+        switch (drawablePosition) {
+            case LEFT:
+                setPadding(horizontalPadding, getPaddingTop(), 0, getPaddingBottom());
+                break;
+            case RIGHT:
+                setPadding(0, getPaddingTop(), horizontalPadding, getPaddingBottom());
+                break;
+            case LEFT_AND_RIGHT:
+                setPadding(horizontalPadding, getPaddingTop(), horizontalPadding, getPaddingBottom());
+                break;
+            default:
+                setPadding(0, getPaddingTop(), 0, getPaddingBottom());
+        }
+    }
+
+    @Override
+    public void setCompoundDrawablesWithIntrinsicBounds(Drawable left, Drawable top, Drawable right, Drawable bottom) {
+        super.setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
+        if (left != null && right != null) {
+            drawableWidth = left.getIntrinsicWidth() + right.getIntrinsicWidth();
+            drawablePosition = DrawablePositions.LEFT_AND_RIGHT;
+        } else if (left != null) {
+            drawableWidth = left.getIntrinsicWidth();
+            drawablePosition = DrawablePositions.LEFT;
+        } else if (right != null) {
+            drawableWidth = right.getIntrinsicWidth();
+            drawablePosition = DrawablePositions.RIGHT;
+        } else {
+            drawablePosition = DrawablePositions.NONE;
+        }
+        requestLayout();
+    }
+}
